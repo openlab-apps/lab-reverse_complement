1b58b3bc4b9de28e0a8cb2df21a2bbcc2e1e61a0
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2022-03-14 15:02:54 +0100

    str conversion

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import sys # function that is requried to read input from command line arguments
 from Bio.Seq import Seq # the package used in our example to generate a reverse complement
 
 # below we define the key function used within a docker
-def revcomp(str): 
-    my_revcomp = Seq(str).reverse_complement()
+def revcomp(input_string): 
+    my_revcomp = Seq(input_string).reverse_complement()
+    my_revcomp = str(my_revcomp)
     return my_revcomp
 
 if __name__ == "__main__":
