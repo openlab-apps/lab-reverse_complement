690402b029687b969f69dd2f4e172c87b7933736
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2022-04-16 19:53:40 +0200

    adding a dummy server scrupt

---
+++ openlab/server.py
@@ -0,0 +1,28 @@
+# POST job_object - check http
+# verify job_object via jobURI - check ipfs
+# check the graph that jobURI is among the  valid_and_active jobURIs - check contract
+# access information from job object and start worker
+# deposit result and metadata to IPFS, generate tokenURI
+# call "swap" contract function that 
+
+## thornii and aakaash: openlab-contract-client
+# subgraph query
+# (future addition by rik) contract interactions
+
+## rik: infrastructure 
+# release toolchest
+# release nextflow client
+# release 
+
+## nice to have
+# estuary client
+
+## config 
+# name
+# handle
+# email
+# terms of use
+# address
+# what-three-words
+# json
+# no warranty research
